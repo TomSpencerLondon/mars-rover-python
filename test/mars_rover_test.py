cfa6a241de068829e4b7b0ad5af989a6a6beba38
Tom <thomas.spencer@codurance.com>
2021-09-09 00:14:50 +0100

    add set up

--- test/mars_rover_test.py
+++ test/mars_rover_test.py
@@ -5,16 +5,16 @@ from src.Rover import Rover
 
 
 class MarsRoverTest(unittest.TestCase):
+    def setUp(self):
+        self.grid = Grid()
+        self.rover = Rover(self.grid)
+
     def test_return_0_0_for_empty_command(self):
-        grid = Grid()
-        rover = Rover(grid)
-        result = rover.execute("")
+        result = self.rover.execute("")
         self.assertEqual("0:0:N", result)
 
     def test_return_0_0_E_for_R(self):
-        grid = Grid()
-        rover = Rover(grid)
-        result = rover.execute("R")
+        result = self.rover.execute("R")
         self.assertEqual("0:0:E", result)
 
 if __name__ == '__main__':
